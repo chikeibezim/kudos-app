77b776f9e5f9071870c05365d686d43a3121c8ea
Chike Ibezim <ibezimchike@gmail.com>
2023-01-26 11:07:30 +0100

    project setup

---
+++ app/root.tsx
@@ -0,0 +1,40 @@
+import type { MetaFunction, LinksFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import styles from './styles/app.css';
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+//export a linksfunction to let remix know you have an asset you want to be imported
+//into all of your modules when the app is built
+
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: styles}]
+}
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //let tailwind know the files to it should look at to apply it's classes
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
