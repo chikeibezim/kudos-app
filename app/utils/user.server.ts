1e633bddfe63b1080b49d3ec0cf908b8a119d374
Chike Ibezim <ibezimchike@gmail.com>
2023-01-29 14:40:25 +0100

    prisma connect

--- app/utils/user.server.ts
+++ app/utils/user.server.ts
@@ -3,12 +3,16 @@ import type { RegisterForm } from './types.server';
 import { prisma } from './prisma.server';
 
 export const createUser = async (user: RegisterForm) => {
-    const passwordHash = await bycrypt.hash(user.password, 10);
+    const passwordHash = await bycrypt.hash(user.password, 10)
     const newUser = await prisma.user.create({ 
         data: {
             email: user.email,
-            password: passwordHash
-            
+            password: passwordHash,
+            profile: {
+                firstName: user.firstName,
+                lastName: user.lastName
+            },
         },
     })
+    return { id: newUser.id, email: user.email }
 }
